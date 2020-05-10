537adf68bd8fc06aa3bc6099874a69c9b499847b
Raido <2370789+raidoa@users.noreply.github.com>
2020-05-10 23:58:24 +0300

    V2.0.0 release

--- lib/Ogone/DirectLink/DirectLinkQueryRequest.php
+++ lib/Ogone/DirectLink/DirectLinkQueryRequest.php
@@ -11,8 +11,8 @@ use Ogone\ShaComposer\ShaComposer;
 class DirectLinkQueryRequest extends AbstractDirectLinkRequest
 {
 
-    const TEST = "https://secure.ogone.com/ncol/test/querydirect_utf8.asp";
-    const PRODUCTION = "https://secure.ogone.com/ncol/prod/querydirect_utf8.asp";
+    const TEST = "https://mdepayments.epdq.co.uk/ncol/test/querydirect_utf8.asp";
+    const PRODUCTION = "https://payments.epdq.co.uk/ncol/prod/querydirect_utf8.asp";
 
     public function __construct(ShaComposer $shaComposer)
     {
